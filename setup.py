7351d7d34cc986b7991e206a505ae31de91cf906
Thomas Schätzlein <thomas@pnxs.de>
2020-10-16 06:12:29 +0200

    prepare for pypi packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from distutils.core import setup
+import setuptools
 
-setup(name='dots',
-      version='1.0',
-      packages=['dots'])
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="dots-cg-pnxs",
+    version="0.0.1",
+    author="Thomas Schaetzlein",
+    author_email="pypi@thomas.pnxs.de",
+    description="DOTS code generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pnxs/dots-code-generator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
