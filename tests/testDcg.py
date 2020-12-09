9751a2d21935d9374c42314d58e1097d99f87f50
Thomas Schätzlein <thomas@pnxs.de>
2020-12-09 09:54:15 +0100

    fix testcases

--- tests/testDcg.py
+++ tests/testDcg.py
@@ -7,12 +7,12 @@ testData1 = """
 import SharedMemStatus
 import SharedMemObjectReference
 
+// Describes an instance of a shared memory object
 struct SharedMemObject 
 {
-    // Describes an instance of a shared memory object
     1: [key] string node;
     2: [key] string id;
-    3: uint32_t size;
+    3: uint32 size;
     4: SharedMemStatus status;
     5: SharedMemObjectReference replicationSrc; // reference to origin, only set when object was replicated
     6: bool partialReplication; 
@@ -22,12 +22,12 @@ struct SharedMemObject
 
 resultData1 = {'imports': ['SharedMemStatus', 'SharedMemObjectReference'], 'enums': [], 'structs': [
     {'name': 'SharedMemObject', 'keys': ['node', 'id'],
-     'structComment': 'Describes an instance of a shared memory object', 'attributes': [
+     'structComment': ['Describes an instance of a shared memory object'], 'attributes': [
         {'cxx_type': 'string', 'name': 'node', 'tag': 1, 'vector': False, 'key': True, 'type': 'string',
          'options': {'key': True}, 'Name': 'Node'},
         {'cxx_type': 'string', 'name': 'id', 'tag': 2, 'vector': False, 'key': True, 'type': 'string',
          'options': {'key': True}, 'Name': 'Id'},
-        {'key': False, 'name': 'size', 'tag': 3, 'vector': False, 'cxx_type': 'uint32_t', 'type': 'uint32_t',
+        {'key': False, 'name': 'size', 'tag': 3, 'vector': False, 'cxx_type': 'uint32', 'type': 'uint32',
          'Name': 'Size'}, {'key': False, 'name': 'status', 'tag': 4, 'vector': False, 'cxx_type': 'SharedMemStatus',
                            'type': 'SharedMemStatus', 'Name': 'Status'},
         {'comment': 'reference to origin, only set when object was replicated', 'cxx_type': 'SharedMemObjectReference',
@@ -52,22 +52,66 @@ enum SpMsgType {
     6: membership,
     7: transition // transition membership message
 }
+
 """
 
-resultData2 = {'enums': [{'items': [{'name': 'regular', 'tag': 1, 'value': 0},
-                                    {'name': 'join', 'tag': 2, 'value': 1},
-                                    {'name': 'leave', 'tag': 3, 'value': 2},
-                                    {'name': 'kill', 'tag': 4, 'value': 3},
-                                    {'name': 'groups', 'tag': 5, 'value': 4},
-                                    {'name': 'membership', 'tag': 6, 'value': 5},
-                                    {'comment': 'transition membership message',
-                                     'name': 'transition',
-                                     'tag': 7,
-                                     'value': 6}],
-                          'name': 'SpMsgType'}],
-               'imports': [],
-               'structs': []}
+resultData2 = {
+    'enums': [
+    {
+        'items': [
+            {'name': 'regular', 'Name': 'Regular', 'tag': 1, 'value': 0},
+            {'name': 'join', 'Name': 'Join', 'tag': 2, 'value': 1},
+            {'name': 'leave', 'Name': 'Leave', 'tag': 3, 'value': 2},
+            {'name': 'kill', 'Name': 'Kill', 'tag': 4, 'value': 3},
+            {'name': 'groups', 'Name': 'Groups', 'tag': 5, 'value': 4},
+            {'name': 'membership', 'Name': 'Membership', 'tag': 6, 'value': 5},
+            {'comment': 'transition membership message', 'name': 'transition', 'Name': 'Transition', 'tag': 7, 'value': 6}
+        ],
+        'Name': 'SpMsgType',
+        'name': 'SpMsgType'
+    }],
+    'imports': [],
+    'structs': []
+}
 
+#################################################################
+
+testData3 = """
+import SharedMemStatus
+import SharedMemObjectReference
+
+struct SharedMemObject 
+{
+    1: [key] string node;
+    2: [key] string id;
+    3: vector<uint32> intVector;
+    4: vector<SharedMemStatus> typeVector;
+}
+
+"""
+
+resultData3 = {
+    'imports': [
+        'SharedMemStatus', 'SharedMemObjectReference'
+    ],
+    'enums': [],
+    'structs': [
+        {'name': 'SharedMemObject', 'keys': ['node', 'id'], 'attributes': [
+            {'cxx_type': 'string', 'name': 'node', 'tag': 1, 'vector': False, 'key': True, 'type': 'string', 'options': {'key': True}, 'Name': 'Node'},
+            {'cxx_type': 'string', 'name': 'id', 'tag': 2, 'vector': False, 'key': True, 'type': 'string', 'options': {'key': True}, 'Name': 'Id'},
+            {'key': False, 'name': 'intVector', 'tag': 3, 'vector': True, 'vector_type': 'uint32', 'cxx_type': 'list', 'cxx_vector_type': 'uint32', 'type': 'vector<uint32>', 'Name': 'IntVector'},
+            {'key': False, 'name': 'typeVector', 'tag': 4, 'vector': True, 'vector_type': 'SharedMemStatus', 'cxx_type': 'list', 'cxx_vector_type': 'SharedMemStatus', 'type': 'vector<SharedMemStatus>', 'Name': 'TypeVector'},
+        ],
+        'keyAttributes': [
+            {'cxx_type': 'string', 'name': 'node', 'tag': 1, 'vector': False, 'key': True, 'type': 'string', 'options': {'key': True}, 'Name': 'Node'},
+            {'cxx_type': 'string', 'name': 'id', 'tag': 2, 'vector': False, 'key': True, 'type': 'string', 'options': {'key': True}, 'Name': 'Id'}
+        ],
+        'options': {}
+        }
+    ]
+}
+
+#################################################################
 
 class TestDotsParser(unittest.TestCase):
     maxDiff = 4096
@@ -75,12 +119,18 @@ class TestDotsParser(unittest.TestCase):
     def test_parsing1(self):
         gen = DdlParser()
         s = gen.parse(testData1)
-        self.assertEqual(s, resultData1)
+        self.assertEqual(resultData1, s)
 
     def test_parsing2(self):
         gen = DdlParser()
         s = gen.parse(testData2)
-        self.assertEqual(s, resultData2)
+        self.assertEqual(resultData2, s)
+
+    def test_parsing3(self):
+        gen = DdlParser()
+        gen.ddlconfig["vector_format"] = "list"
+        s = gen.parse(testData3)
+        self.assertEqual(resultData3, s)
         
 
 if __name__ == '__main__':
